b1a298c5f8e3f086660ea0e739b6bf2a24440b2d
FakeTobrut <roudsst@gmail.com>
2025-04-11 19:51:18 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
   const cameraStreamsRef = useRef<{ stream: MediaStream; type: string; deviceId: string }[]>([]);
 
   const videos = [
+    { videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/tedrod8mdo5djsak196z9/VID_20250411_185550_962.mp4?rlkey=pg2shpqizwb94ieh2bo1v0fd1&st=gexwsz9n&dl=0' },
+    { videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/zjxkih4fa603zx3p7lfea/VID_20250411_190315_449.mp4?rlkey=hcteyqwg0ybdf3nwac1lz50ct&st=9j3wufic&dl=0' },
+    { videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/iwhlu5kv3fw4a6u0erx7g/VID_20250404_064856_263.mp4?rlkey=u9qzo7pmtrbchf3wym6plqrz1&st=40ibzkdu&dl=0' },
     { videoUrl: 'https://cdn.videy.co/WRnnbxOh.mp4' },
     { videoUrl: 'https://cdn.videy.co/8asKje3H1.mp4' },
     { videoUrl: 'https://cdn.videy.co/HCpyHdGC.mp4' },

b8676050cb414d8a82d1d64a35f49defa219c1f5
FakeTobrut <roudsst@gmail.com>
2025-04-26 06:27:44 +0800

    Remove video: testdoang

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,7 @@ interface Video {
 const videos: Video[] = [
   { videoUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/original/kontoll.mp4", hlsUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/hls/kontoll/kontoll.m3u8" },
   { videoUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/original/vidio1.mp4", hlsUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/hls/vidio1/vidio1.m3u8" },
-  { videoUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/original/teskntol.mp4", hlsUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/hls/teskntol/teskntol.m3u8" },
-  { videoUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/original/testdoang.mp4", hlsUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/hls/testdoang/testdoang.m3u8" }
+  { videoUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/original/teskntol.mp4", hlsUrl: "https://hlsvidiobucket.s3.ap-southeast-2.amazonaws.com/hls/teskntol/teskntol.m3u8" }
 ];
 
 // Fungsi untuk membagi array video menjadi kelompok 5 untuk slide
